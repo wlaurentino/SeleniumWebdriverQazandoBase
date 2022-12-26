29c8ae42904b4d738d5bb6f9ccc88f05ffd6da21
wlaurentio <gama.william@gmail.com>
2022-12-26 21:30:58 +0000

    first commig

---
+++ src/test/java/support/Utils.java
@@ -0,0 +1,20 @@
+package support;
+
+import runner.RunCucumber;
+
+import java.util.Random;
+
+public class Utils extends RunCucumber {
+
+    public static String getRandomEmail() {
+        String email_init = "qazando_";
+        String email_final = "@qazando.com.br";
+
+        Random random = new Random();
+        int minimo = 1;
+        int maximo = 999999999;
+        int resultado = random.nextInt(maximo-minimo) + minimo;
+
+        return email_init + resultado + email_final;
+    }
+}
